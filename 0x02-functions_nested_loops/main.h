3273cbda01f9992be6bf74b59e0b45e5a5126596
spider067 <spideranon067@gmail.com>
2023-06-21 11:25:45 -0700

    readme file created

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _putchar(char c);
+
+/* prints alphabet in lowercase */
+void print_alphabet(void);
+/* prints the alphabets in small letters 10 times */
+void print_alphabet_x10(void);
+/* checks for lowercase characters */
+int _islower(int c);
+/* check for alphabetic characters */
+int _isalpha(int c);
+/* prints sign of a number */
+int print_sign(int n);
+/* gets absolute value */
+int _abs(int);
+/* prints last digit */
+int print_last_digit(int);
+/* prints every minute of the day of jack bauer */
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+#endif /* MAIN_H */
